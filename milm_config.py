b074dc14dc28b368c6f8d0d0af27dcb8950b1d0f
Brendan O'Connor <brendan@maliceafterthought.com>
2022-03-23 14:04:24 -0700

    Adding generic data.

---
+++ milm_config.py
@@ -0,0 +1,27 @@
+# Yes, I should use YAML or something.
+# No, I'm not going to.
+
+# Line 1 of the label text
+LINE_ONE = "GENERIC HOSPITAL"
+
+# Line 2 of the label text
+LINE_TWO = "GENERIC TOWN"
+
+# Medical Record Number
+MRN = "0000000000"
+
+# Patient Name
+PATIENT = "Bobison, Alice"
+
+# Patient DOB
+DOB = "01/19/38"
+
+# Item on the label
+CONTENTS = "Breast Milk"
+
+# Data contained in the AZTEC barcode on the label
+BARCODE_DATA = "Hello I am a barcode"
+
+# For the label printer (I use a Brother QL-800)
+# Should be "usb://<vendorid>:<productid>", like "usb://0x04f9:0x209b"
+PRINTER_IDENTIFIER = "usb://0x04f9:0x209b"
